05a97062a06fde6cfc7a0f7aba5fa25e2214158d
Herian Cavalcante <herian.cavalcante@live.com>
2023-11-02 18:30:00 -0300

    att

--- trabalho_final/classes.py
+++ trabalho_final/classes.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractclassmethod
 from forca import forca
 import random
+from os import system
+from time import sleep
 
 # Classes que serão importadas para o arquivo main.py e será molde para os temas do jogo e instanciação do jogador
 
@@ -39,6 +41,7 @@ class Estatistica(Tema):
         print('NÍVEL FÁCIL'.center(50))
 
         for i, palavra in enumerate(self.palavras_faceis, start=1):
+            system('cls')
             chances = 7
             print(f'Você tem {chances} chances')
             print(f'\n\nRodada {i}'.center(50))
@@ -48,6 +51,7 @@ class Estatistica(Tema):
             if not resultado[0]:
                 print(f'Jogo encerrado! Você jogou {i} rodadas')
                 return False, pontos_no_nivel
+            sleep(2)
         return True, pontos_no_nivel
 
     def nivel_medio(self):
@@ -57,6 +61,7 @@ class Estatistica(Tema):
         print('NÍVEL MÉDIO'.center(50))
 
         for i, palavra in enumerate(self.palavras_medias, start=1):
+            system('cls')
             chances = 7
             print(f'Você tem {chances} chances')
             print(f'\n\nRodada {i}'.center(50))
@@ -66,6 +71,7 @@ class Estatistica(Tema):
             if not resultado[0]:
                 print(f'Jogo encerrado! Você jogou {i} rodadas')
                 return False, pontos_no_nivel
+            sleep(2)
         return True, pontos_no_nivel
 
     def nivel_dificil(self):
@@ -75,6 +81,7 @@ class Estatistica(Tema):
         print('NÍVEL DIFÍCIL!'.center(50))
 
         for i, palavra in enumerate(self.palavras_dificeis, start=1):
+            system('cls')
             chances = 7
             print(f'Você tem {chances} chances')
             print(f'\n\nRodada {i}'.center(50))
@@ -84,6 +91,7 @@ class Estatistica(Tema):
             if not resultado[0]:
                 print(f'Jogo encerrado! Você jogou {i} rodadas')
                 return False, pontos_no_nivel
+            sleep(2)
         return True, pontos_no_nivel
 
 
@@ -102,6 +110,7 @@ class Geociencias(Tema):
         pontos_no_nivel = 0
 
         for i, palavra in enumerate(self.palavras_faceis, start=1):
+            system('cls')
             chances = 7
             print(f'Você tem {chances} chances')
             print(f'\n\nRodada {i}'.center(50))
@@ -111,19 +120,52 @@ class Geociencias(Tema):
             if not resultado[0]:
                 print(f'Jogo encerrado! Você jogou {i} rodadas')
                 return False, pontos_no_nivel
+            sleep(2)
         return True, pontos_no_nivel
 
     def nivel_medio(self):
-        ...
+        # Embaralhando a ordem das palavras
+        random.shuffle(self.palavras_medias)
+        pontos_no_nivel = 0
+
+        for i, palavra in enumerate(self.palavras_medias, start=1):
+            system('cls')
+            chances = 7
+            print(f'Você tem {chances} chances')
+            print(f'\n\nRodada {i}'.center(50))
+            resultado = forca(palavra, chances, 250)
+            pontos_no_nivel += resultado[1]
+
+            if not resultado[0]:
+                print(f'Jogo encerrado! Você jogou {i} rodadas')
+                return False, pontos_no_nivel
+            sleep(2)
+        return True, pontos_no_nivel
 
     def nivel_dificil(self):
-        ...
+        # Embaralhando a ordem das palavras
+        random.shuffle(self.palavras_dificeis)
+        pontos_no_nivel = 0
+
+        for i, palavra in enumerate(self.palavras_dificeis, start=1):
+            system('cls')
+            chances = 7
+            print(f'Você tem {chances} chances')
+            print(f'\n\nRodada {i}'.center(50))
+            resultado = forca(palavra, chances, 250)
+            pontos_no_nivel += resultado[1]
+
+            if not resultado[0]:
+                print(f'Jogo encerrado! Você jogou {i} rodadas')
+                return False, pontos_no_nivel
+            sleep(2)
+        return True, pontos_no_nivel
 
 
 class Jogador:
     # Classe para instanciar o jogador e armazenar sua pontuação
-    def __init__(self, nome='Anônimo') -> None:
-        self.nome = nome
+    def __init__(self, nome) -> None:
+        self.nome = nome if nome != '' else 'Anônimo'
         self.__pontuacao = 0  # Atributo privado para evitar que o jogador tenha acesso
 
     @property
--- trabalho_final/main.py
+++ trabalho_final/main.py
@@ -1,18 +1,46 @@
-from classes import Estatistica, Geociencias
+import classes
+from os import system
 pts = 0
 
+escolha = input(
+    'JOGO DA FORCA - MENU\n1 - Estatística\n2 - Geociências\n3 - Sair\n')
 
-est = Estatistica()
+match escolha:
+    case '1':
+        jogo = classes.Estatistica()
 
-resultado = est.nivel_facil()
-pts += resultado[1]
+        resultado = jogo.nivel_facil()
+        pts += resultado[1]
 
-if resultado[0]:
-    resultado = est.nivel_medio()
-    pts += resultado[1]
+        if resultado[0]:
+            resultado = jogo.nivel_medio()
+            pts += resultado[1]
 
-if resultado[0]:
-    resultado = est.nivel_dificil()
-    pts += resultado[1]
+        if resultado[0]:
+            resultado = jogo.nivel_dificil()
+            pts += resultado[1]
+    case '2':
+        jogo = classes.Geociencias()
 
-print(f'A pontuação final é: {pts}')
+        resultado = jogo.nivel_facil()
+        pts += resultado[1]
+
+        if resultado[0]:
+            resultado = jogo.nivel_medio()
+            pts += resultado[1]
+
+        if resultado[0]:
+            resultado = jogo.nivel_dificil()
+            pts += resultado[1]
+    case _:
+        print('Entrada inválida!')
+
+
+nome = input('\n\nDigite o seu nome: ')
+
+jogador = classes.Jogador(nome)
+jogador.adicionar_pontuacao(pts)
+
+system('cls')
+print('PONTUAÇÃO FINAL'.center(50, '-'))
+print(f'JOGADOR: {jogador.nome} - {jogador.pontuacao} PTS')
